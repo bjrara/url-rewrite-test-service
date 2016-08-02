c3abc4f8a8451b835c3b9646a0cdd5a8ebdb5a42
bjrara <bjrara@sina.com>
2016-08-02 23:03:14 +0800

    add imitate handler

--- server.js
+++ server.js
@@ -1,32 +1,34 @@
-var handler = require("./anonymous_handler");
-var http = require("http");
-var https = require("https");
-var fs = require("fs");
-var url = require("url");
+var http = require('http');
+var https = require('https');
+var url = require('url');
+
+var handler = require('./imitate.response.handler')
 
 function start_http(port) {
-  function onRequest(request, response) {
-    if (request.url.indexOf('/favicon.ico') === 0)
-      return;
-    handler.handleRequest(request, response);
-  }
-  http.createServer(onRequest).listen(port);
-  console.log("http server has started.");
+    function onRequest(request, response) {
+        if (request.url.indexOf('/favicon.ico') === 0) return;
+        handler.handleRequest(request, response);
+    }
+    
+    http.createServer(onRequest).listen(port);
+    console.log('http server has started.');
 }
 
 function start_https(port) {
-  function onRequest(request, response) {
-    if (request.url.indexOf('/favicon.ico') === 0)
-      return;
-    handler.handleRequest(request, response);
-  }
+    function onRequest(request, response) {
+        if (request.url.indexOf('/favicon.ico') === 0) return;
+        handler.handleRequest(request, response);
+    }
 	
+    var fs = require('fs');
 	var options = {
-		key: fs.readFileSync('cert/ca.key'),
-		cert: fs.readFileSync('cert/ca.crt')
+		key: fs.readFileSync('ssl/server.key'),
+		cert: fs.readFileSync('ssl/server.crt')
 	}
+    
 	https.createServer(options, onRequest).listen(port);
-  console.log("https server has started.");
+    console.log('https server has started.');
 }
+
 exports.start_http = start_http;
 exports.start_https = start_https;
